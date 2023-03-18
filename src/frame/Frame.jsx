213e637882054be3d22e55ecc73811f3e54c2c95
Christian Tognazza <tognazzachristian@gmail.com>
2023-03-18 08:59:16 +0100

    chrigeeel: tailwind automatic class sorting

--- src/frame/Frame.jsx
+++ src/frame/Frame.jsx
@@ -3,9 +3,9 @@ import { Outlet } from "react-router-dom";
 
 const Frame = () => {
   return (
-    <div className="flex w-screen h-screen">
-      <div className="hidden w-64 md:block bg-gray-700">Sidebar</div>
-      <div className="flex h-screen flex-col w-full overflow-hidden">
+    <div className="flex h-screen w-screen">
+      <div className="hidden w-64 bg-gray-700 md:block">Sidebar</div>
+      <div className="flex h-screen w-full flex-col overflow-hidden">
         <Outlet />
       </div>
     </div>
